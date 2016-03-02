878977b3d3a829fc6ea0039a51fb5aec999126ee
Jeff Linton <jslinton44@gmail.com>
2016-03-01 23:04:06 -0700

    Updates the sample batch script

--- run-watcher-sample.bat
+++ run-watcher-sample.bat
@@ -1,1 +1,10 @@
+:: This script will not work if the project has not been built
+:: It's easy to verify, simply go to the Debug directory below
+
+@echo off
+
+SET exePath=UtahRealEstateWatcher/UtahRealEstateWatcher/bin/Debug
+
+cd %exePath%
+
 UtahRealEstateWatcher "Herriman;Lehi;Draper;Saratoga Springs;Riverton;South Jordan" 200000 280000
